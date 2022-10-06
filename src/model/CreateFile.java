5e2e660d4155302e869198f26cbd0749321f55ed
davidU01 <david.abril@uptc.edu.co>
2022-10-06 11:26:25 -0500

    Properties

--- src/model/CreateFile.java
+++ src/model/CreateFile.java
@@ -1,11 +1,14 @@
 package model;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Properties;
 
 public class CreateFile {
 
@@ -15,11 +18,16 @@ public class CreateFile {
 			@Override
 			public void run() {
 				while(true) {
+					Properties properties= new Properties();
+					try {
+						properties.load(new FileInputStream(new File("./taller/configuration.properties")));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
 					DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
 					String contenido = dtf.format(LocalDateTime.now());
-
-					try(FileWriter fw = new FileWriter("./taller/fechas.txt", true);
+					try(FileWriter fw = new FileWriter(properties.getProperty("PATH")+properties.getProperty("NAME"), true);
 							BufferedWriter bw = new BufferedWriter(fw);
 							PrintWriter out = new PrintWriter(bw)) {
 						out.println(contenido);
@@ -31,6 +39,7 @@ public class CreateFile {
 				}
 			}
 		}).start();
+	
 	}
 
 }
